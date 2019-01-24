1d35b44d4d86540b1287a312a6f8d2f96152150b
GrzegorzTomicki <grzegorztomicki@gmail.com>
2019-01-24 16:58:33 +0100

    Added contacs table to the database

---
+++ database/People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NULL, 
+    [Company] NCHAR(50) NULL, 
+    [Telephone] NCHAR(50) NULL, 
+    [Email] NCHAR(50) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL
+)
